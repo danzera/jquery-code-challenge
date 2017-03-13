2f07f487b928cd80560c9bf8a092499d6d4a101b
Dan Zera <dczera@gmail.com>
2017-03-13 13:58:40 -0500

    append elements inside the generated <div>'s

--- client.js
+++ client.js
@@ -1,5 +1,19 @@
+// counter to track button-clicks
+var clicks = 0;
+
 $(document).ready(function() {
-  $('button').on('click', function() {
-    $('.text-area').append('<div class="generator"></div>');
+  // on generate button click
+  $('.generate').on('click', 'button', function() {
+    // increment the clicks counter
+    clicks++;
+    // append a <div> to the text-area <div>
+    $('.text-area').append('<div class="shell"></div>');
+    // store our newly created <div> in a varialbe
+    var $el = $('.text-area').children().last();
+    // append a paragraph to our new <div> displaying the number of button clicks
+    $el.append('<p class="clicks">' + clicks + '</p>');
+    $el.append('<button>Swap</button>');
+    $el.append('<button>Delete</button>');
+    console.log($el);
   });
 });
