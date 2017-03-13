0ac11c865582ec889954e559a888870b252a1adc
Dan Zera <dczera@gmail.com>
2017-03-13 14:14:20 -0500

    add delete button functionality

--- client.js
+++ client.js
@@ -10,15 +10,19 @@ $(document).ready(function() {
     $('.text-area').append('<div class="shell"></div>');
     // store our newly created <div> in a varialbe
     var $el = $('.text-area').children().last();
-    // append a paragraph to our new <div> displaying the number of button clicks
+    // append a paragraph to our new <div> displaying number of 'generate' button clicks
     $el.append('<p class="clicks">' + clicks + '</p>');
+    // append a swap button for changing the background-color
     $el.append('<button class="swap">Swap</button>');
+    // append a delete button for removing the <div>
     $el.append('<button class="delete">Delete</button>');
-    console.log($el);
   });
 
   $('.text-area').on('click', '.swap', function() {
-    console.log('here');
     $(this).parent().addClass('yellow');
+  });
+
+  $('.text-area').on('click', '.delete', function() {
+    $(this).parent().remove();
   });
 });
